0dbfb7a71bef9e24280f5c3c15a53e5cdcb3ff9e
gccpacman <gccpacman@gmail.com>
2020-05-04 02:21:48 +0800

    Flutter Layouts Challenge finished

--- lib/main.dart
+++ lib/main.dart
@@ -12,29 +12,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Row(
-            verticalDirection: VerticalDirection.down,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.end,
+//            verticalDirection: VerticalDirection.down,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
 //            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Icon(Icons.star),
-              Icon(Icons.star),
-              SizedBox(width: 30.0),
-              Icon(Icons.star),
               Container(
-                width: 30.0,
-                height: 100.0,
                 color: Colors.red,
-                child: Icon(Icons.star, color: Colors.white,),
+                width: 100.0,
+                child: Icon(Icons.add, )
               ),
               Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.yellow,
+                        width: 100.0,
+                        height: 100.0,
+                      ),
+                      Container(
+                        color: Colors.green,
+                        width: 100.0,
+                        height: 100.0,
+                      ),
+                    ],
+                  )
+              ),
+              Container(
+                color: Colors.blue,
                 width: 100.0,
-                height: 100.0,
-                color: Colors.yellow,
-                child: Icon(Icons.star, color: Colors.blue,),
+                child: Icon(Icons.star, color: Colors.black,),
               )
             ],
           )
