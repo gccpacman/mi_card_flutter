ca2583744225f389e947dbb7363e53e3395f7c69
gccpacman <gccpacman@gmail.com>
2020-05-04 19:53:03 +0800

    phone number and email address

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(
@@ -14,61 +15,91 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
+//            child: Row(
 //            verticalDirection: VerticalDirection.down,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//            mainAxisAlignment: MainAxisAlignment.spaceBetween,
 //            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                color: Colors.red,
-                width: 30.0,
-                child: Icon(Icons.add, )
-              ),
-              Column(
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 50.0,
-                    backgroundImage: AssetImage('images/saul.jpeg'),
-                    backgroundColor: Colors.blue,
-                  ),
-                  Text(
-                    'Saul Goodman',
+//          children: <Widget>[
+//              Container(
+//                color: Colors.red,
+//                width: 30.0,
+//                child: Icon(Icons.add, )
+//              ),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+//                crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/saul.jpeg'),
+                  backgroundColor: Colors.blue,
+                ),
+                Text('Saul Goodman',
                     style: TextStyle(
-                      fontSize: 40,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Pacifico',
-                      color: Colors.yellow
-                    )
-                  ),
-                  Text(
-                    'Justice matters most!',
+                        fontSize: 40,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Pacifico',
+                        color: Colors.yellow)),
+                Text('Justice matters most!',
                     style: TextStyle(
                       fontFamily: 'Cascadia',
                       color: Colors.white,
                       fontSize: 20.0,
                       letterSpacing: 2.0,
-                    )
-                  ),
-                  Container(
+                    )),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                    padding: EdgeInsets.all(10.0),
+                    color: Colors.white,
                     child: Row(
                       children: <Widget>[
-                        Icon(
-
+                        Icon(Icons.phone, color: Colors.teal),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(
+                          '505-842-5662',
+                          style: TextStyle(
+                              color: Colors.teal,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold),
                         )
                       ],
-                    )
-                  )
-                ],
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-              Container(
-                color: Colors.blue,
-                width: 30.0,
-                child: Icon(Icons.star, color: Colors.black,),
-              )
-            ],
-          )
+                    )),
+                Container(
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                    padding: EdgeInsets.all(10.0),
+                    color: Colors.white,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.email, color: Colors.teal),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(
+                          'bettercallsaul@gmail.com',
+                          style: TextStyle(
+                              color: Colors.teal,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ))
+              ],
+            ),
+//              Container(
+//                color: Colors.blue,
+//                width: 30.0,
+//                child: Icon(Icons.star, color: Colors.black,),
+//              )
+//          ],
+//        )
 //          child: Container(
 //            child: Text('This is some words.'),
 //            color: Colors.yellow,
@@ -77,7 +108,7 @@ class MyApp extends StatelessWidget {
 //            margin: EdgeInsets.all(20.0),
 //            padding: EdgeInsets.all(10.0),
 //          )
-        ),
+            ),
 //        appBar: AppBar(
 //          backgroundColor: Colors.blue,
 //          title: Text(
